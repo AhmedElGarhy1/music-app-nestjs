a1aa6246157005e05ee195753c5b7aa51218bd3e
AhmedElGarhy1 <gemater.g@gmail.com>
2023-09-17 20:59:38 +0200

    update relations

--- src/modules/singer-album/singer-album.entity.ts
+++ src/modules/singer-album/singer-album.entity.ts
@@ -1,9 +1,12 @@
 import { AbstractAlbum } from 'src/common/classes/abstract-album';
-import { Entity, ManyToOne } from 'typeorm';
+import { Column, Entity, ManyToOne } from 'typeorm';
 import { Singer } from '../singer/singer.entity';
 
 @Entity()
 export class SingerAlbum extends AbstractAlbum {
   @ManyToOne(() => Singer, (singer) => singer.singerAlbums)
   singer: Singer;
+
+  @Column()
+  singer_id: number;
 }
